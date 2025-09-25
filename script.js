f6fe43803e07c85bc2956e439f167a3240c66c8e
shreyashmahalle <128777174+shreyashmahalle@users.noreply.github.com>
2025-09-25 10:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-    const container = document.getElementById('container');
-    const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
-    const SQUARES = 800;
-
-    for (let i = 0; i < SQUARES; i++) {
-      const square = document.createElement('div');
-      square.classList.add('square');
-
-      square.addEventListener('mouseover', () => setColor(square));
-      square.addEventListener('mouseout', () => removeColor(square));
-
-      container.appendChild(square);
-    }
-
-    function setColor(element) {
-      const color = getRandomColor();
-      element.style.background = color;
-    }
-
-    function removeColor(element) {
-      element.style.background = '#1d1d1d';
-    }
-
-    function getRandomColor() {
-      return colors[Math.floor(Math.random() * colors.length)];
-    }
+let container = document.querySelector('.container');
+for (let i = 0; i < 800; i++) {
+	let square = document.createElement('div');
+	square.classList.add('square');
+	square.addEventListener('mouseover',()=>{
+		square.style.backgroundColor = "greenyellow";
+		square.style.transition = "none";
+	})
+	square.addEventListener('mouseout',()=>{
+		square.style.backgroundColor = "rgb(29, 29, 29)";
+		square.style.transition = "background-color 1s ease";
+	})
+	container.appendChild(square);
+}
